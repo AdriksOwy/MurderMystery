3ab3fede4f103ab75ef0b9d396d02ab5a399d064
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-27 20:05:00 +0100

    init

---
+++ src/main/java/com/murder/Murder.java
@@ -0,0 +1,17 @@
+package com.murder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Murder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
