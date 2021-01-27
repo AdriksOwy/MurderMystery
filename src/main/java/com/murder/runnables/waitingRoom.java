995026427d59afba249cf31b3c00b8278ed40bc9
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-27 20:35:38 +0100

    Added waitingRoom

---
+++ src/main/java/com/murder/runnables/waitingRoom.java
@@ -0,0 +1,14 @@
+package com.murder.runnables;
+
+import org.bukkit.World;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class waitingRoom extends BukkitRunnable {
+    private final World world;
+    public waitingRoom(World world) {
+        this.world = world;
+    }
+    public void run() {
+
+    }
+}
