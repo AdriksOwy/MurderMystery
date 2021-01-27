0cf9aeb8564da504c15876ce368830ec08d5b505
Adriks <lepekadrian123@gmail.com>
2021-01-27 20:23:11 +0100

    main, register

--- src/main/java/com/murder/Murder.java
+++ src/main/java/com/murder/Murder.java
@@ -1,5 +1,6 @@
 package com.murder;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Murder extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class Murder extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger () . info ( ChatColor . YELLOW  +  " [MurderMystery] Plugin jest wlaczony! " );
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger () . info ( ChatColor. RED  +  " [MurderMystery] Plugin jest wylaczony! " );
     }
 }
