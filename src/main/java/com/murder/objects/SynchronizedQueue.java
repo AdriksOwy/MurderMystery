1af0c8ea7a6aa9defb2cb28f839ff72af303d7b9
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-28 10:04:14 +0100

    new size method for SynchronizedQueue

--- src/main/java/com/murder/objects/SynchronizedQueue.java
+++ src/main/java/com/murder/objects/SynchronizedQueue.java
@@ -6,11 +6,15 @@ import java.util.NoSuchElementException;
 public class SynchronizedQueue<T> {
     private LinkedList<T> list;
 
-    public synchronized void push(T value){
-        list.add(value);
+    public synchronized void push(T o){
+        list.add(o);
     }
     public synchronized T pop() throws NoSuchElementException {
         return list.pop();
+    }
+    public int size()
+    {
+        return list.size();
     }
 
     public SynchronizedQueue(){
