fe2bc25a8128a6558f1f8de4081631de39901b53
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-28 09:46:29 +0100

    SynchronizedQueue fix push method

--- src/main/java/com/murder/objects/SynchronizedQueue.java
+++ src/main/java/com/murder/objects/SynchronizedQueue.java
@@ -7,7 +7,7 @@ public class SynchronizedQueue<T> {
     private LinkedList<T> list;
 
     public synchronized void push(T value){
-        list.push(value);
+        list.add(value);
     }
     public synchronized T pop() throws NoSuchElementException {
         return list.pop();
