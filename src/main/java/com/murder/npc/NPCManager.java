68f6a009fbf42a7aeaffb71bb0d6824ab32e717c
Adriks <lepekadrian123@gmail.com>
2021-01-28 19:04:23 +0100

    editing plugin.yml, create new command

--- src/main/java/com/murder/npc/NPCManager.java
+++ src/main/java/com/murder/npc/NPCManager.java
@@ -1,6 +1,6 @@
 package com.murder.npc;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
@@ -13,13 +13,13 @@ public class NPCManager {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if(cmd.getName().equalsIgnoreCase("SetVillager")) {
+        if(cmd.getName().equalsIgnoreCase("npccreate")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if(player.hasPermission("npc")) {
+                if (player.hasPermission("npc")) {
                     Villager villager = (Villager) player.getWorld().spawnEntity(player.getLocation(), EntityType.VILLAGER);
                     villager.setCustomName("Murder Mystery");
-                    player.sendMessage(Color.RED + "Villager was created!");
+                    player.sendMessage(ChatColor.RED + "Villager was created!");
                 }
             }
         }
