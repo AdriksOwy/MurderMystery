81977bf7ba09180a205ac2bdf9a62a6d5e6d7b38
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-01-28 11:33:37 +0100

    create WorldManager class

---
+++ src/main/java/com/murder/WorldManager.java
@@ -0,0 +1,49 @@
+package com.murder;
+
+import jdk.tools.jlink.plugin.Plugin;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.WorldCreator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+;
+
+public class WorldManager {
+   public JavaPlugin plugin;
+
+    public WorldManager(JavaPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    public void createWorld(String world){
+        File dataFolder = new File(plugin.getDataFolder().getPath());
+        String strData = dataFolder.toString();
+
+        String[] split = strData.toString().split(File.pathSeparator);
+        String rootFolder = split[split.length - 3];
+
+        File root = new File(rootFolder);
+        File srcDir = new File(root+File.pathSeparator+"backup");
+
+        if (!srcDir.exists()) {
+            Bukkit.getLogger().warning("Backup does not exist!");
+            return;
+        }
+
+        File destDir = new File(root+File.pathSeparator+world);
+
+        try {
+            FileUtils.copyDirectory(srcDir, destDir);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        Bukkit.getServer().createWorld(new WorldCreator(world));
+    }
+
+
+
+
+}
