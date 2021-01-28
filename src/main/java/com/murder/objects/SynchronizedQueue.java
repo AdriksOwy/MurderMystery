40c30b5ad40c7683286867d3ae760287de7636f9
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-28 09:11:21 +0100

    new class SynchronizedQueue

---
+++ src/main/java/com/murder/objects/SynchronizedQueue.java
@@ -0,0 +1,19 @@
+package com.murder.objects;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class SynchronizedQueue<T> {
+    private LinkedList<T> list;
+
+    public synchronized void push(T value){
+        list.push(value);
+    }
+    public synchronized T pop() throws NoSuchElementException {
+        return list.pop();
+    }
+
+    public SynchronizedQueue(){
+        list = new LinkedList<T>();
+    }
+}
