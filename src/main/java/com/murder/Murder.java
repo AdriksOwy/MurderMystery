4cc579e28f811263653286e3353ebd9435ccc1ee
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-28 08:17:57 +0100

    Added .getMainPlugin()

--- src/main/java/com/murder/Murder.java
+++ src/main/java/com/murder/Murder.java
@@ -4,16 +4,18 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Murder extends JavaPlugin {
-
+    static JavaPlugin plugin;
+    public static JavaPlugin getMainPlugin() {
+        return plugin;
+    }
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
         getLogger().info( ChatColor.YELLOW + " [MurderMystery] Plugin jest wlaczony! " );
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
         getLogger().info(ChatColor.RED + " [MurderMystery] Plugin jest wylaczony! " );
     }
 }
