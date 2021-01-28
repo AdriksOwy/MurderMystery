7aac9de0cecd0161d2ce8ac2b63b94fb29a66feb
Adriks <lepekadrian123@gmail.com>
2021-01-28 21:35:49 +0100

    delete a class npcmanager and code copy to main

--- src/main/java/com/murder/Murder.java
+++ src/main/java/com/murder/Murder.java
@@ -1,6 +1,13 @@
 package com.murder;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Murder extends JavaPlugin {
@@ -23,4 +30,31 @@ public final class Murder extends JavaPlugin {
 
         getLogger().info(ChatColor.RED + " [MurderMystery] Plugin is disable! " );
     }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if(cmd.getName().equalsIgnoreCase("SetVillager")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission("test.setvillager")) {
+                    Villager villager = (Villager) player.getWorld().spawnEntity(player.getLocation(), EntityType.VILLAGER);
+                    villager.setCustomName("Murder Mystery");
+                    player.sendMessage(ChatColor.RED + "Villager was created!");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @EventHandler
+        public void on(PlayerInteractEntityEvent event) {
+            Player player = event.getPlayer();
+            if(event.getRightClicked() instanceof Villager) {
+               Villager villager = (Villager) event.getRightClicked();
+               if(villager.getCustomName().equalsIgnoreCase("Murder Mystery")) {
+                    player.sendMessage(ChatColor.GREEN + "Omg kurwa dziala!");
+               }
+            }
+        }
 }
