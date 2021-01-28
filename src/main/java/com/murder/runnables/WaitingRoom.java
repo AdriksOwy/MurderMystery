b6fb09d1d31fd06bdb954aa0511255677e48bd64
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-28 10:20:37 +0100

    syntax fix (class waitingRoom -> class WaitingRoom)

--- src/main/java/com/murder/runnables/WaitingRoom.java
+++ src/main/java/com/murder/runnables/WaitingRoom.java
@@ -3,9 +3,9 @@ package com.murder.runnables;
 import org.bukkit.World;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class waitingRoom extends BukkitRunnable {
+public class WaitingRoom extends BukkitRunnable {
     private final World world;
-    public waitingRoom(World world) {
+    public WaitingRoom(World world) {
         this.world = world;
     }
     public void run() {
