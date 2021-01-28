476afda90b8cb9780fa51fa8adb7c7e63d7ae057
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-28 09:30:29 +0100

    new ExampleTest.java

---
+++ src/test/java/com/murder/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.murder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class ExampleTest {
+    @Test
+    public void testExample(){
+        assertTrue(true);
+    }
+}
