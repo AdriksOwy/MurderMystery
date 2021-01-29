79a8cab114f22fb91ea13e8d259fab13dc4ad4ab
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-29 22:01:42 +0100

    refactor: new MurderNPCEvent class

---
+++ src/main/java/com/murder/events/MurderNPCEvent.java
@@ -0,0 +1,20 @@
+package com.murder.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class MurderNPCEvent implements Listener {
+    @EventHandler
+    public void on (PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer ();
+        if (event.getRightClicked () instanceof Villager) {
+            Villager villager = (Villager) event.getRightClicked ();
+            if (villager.getCustomName (). equalsIgnoreCase ("Murder")) {
+                player.sendMessage("O kurwa dziala!");
+            }
+        }
+    }
+}
