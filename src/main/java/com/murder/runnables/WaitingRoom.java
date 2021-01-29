c78fab54c78bc50d196f9bc8b0ef728587e7e19e
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-29 22:02:26 +0100

    feat: counter is working now

--- src/main/java/com/murder/runnables/WaitingRoom.java
+++ src/main/java/com/murder/runnables/WaitingRoom.java
@@ -12,46 +12,56 @@ public class WaitingRoom extends BukkitRunnable {
     private final World world;
     private final int stage;
     private int time;
+    private int delay;
     public WaitingRoom(World world, int stage) {
         this.world = world;
         this.stage = stage;
     }
     public void run() {
-        List<Player> players = world.getPlayers();
-        int mods = 0;
-
-        updateTime();
-        for (Player player : players) {
-            if (player.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
-                mods++;
-            }
-        }
-        int playersCount = players.size() - mods;
-        if (playersCount >= 8) {
-            new WaitingRoom(world, stage-1).runTaskAsynchronously(Murder.getMainPlugin());
-        } else {
-            new WaitingRoom(world, 5).runTaskLaterAsynchronously(Murder.getMainPlugin(), 5 * 20);
-        }
-    }
-
-    private void updateTime(){
         switch (stage) {
             case 1:
                 time = 60;
+                delay = 30;
                 break;
             case 2:
                 time = 30;
+                delay = 15;
                 break;
             case 3:
                 time = 15;
+                delay = 5;
                 break;
             case 4:
                 time = 10;
+                delay = 5;
                 break;
             case 5:
                 time = 5;
+                delay = 5;
                 break;
         }
+        List<Player> players = world.getPlayers();
+        if (stage != 6) {
+            int mods = 0;
+            for (Player player : players) {
+                if (player.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
+                    mods++;
+                }
+            }
+            int playersCount = players.size() - mods;
+            if (playersCount >= 1) {
+                players.forEach(player -> player.sendMessage("Gra rozpocznie się za "+time+" sekund"));
+                new WaitingRoom(world, stage + 1).runTaskLaterAsynchronously(Murder.getMainPlugin(), delay * 20);
+            }
+            else {
+                new WaitingRoom(world, 1).runTaskLaterAsynchronously(Murder.getMainPlugin(), 5 * 20);
+                if (stage != 1) {
+                    players.forEach(player -> player.sendMessage("Rozpoczecie gry zostalo przerwane"));
+                }
+            }
+        } else {
+            players.forEach(player -> player.sendMessage("Gra właśnie się rozpoczęła"));
+        }
+
     }
 }
-
