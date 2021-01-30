d72a1503b5401e5621df6970cc6760a448f83284
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-30 21:54:40 +0100

    refactor: new structure for an implementation of CommandRunner and future factory

---
+++ src/main/java/com/murder/commands/CommandData.java
@@ -0,0 +1,18 @@
+package com.murder.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+public class CommandData {
+    public CommandSender sender;
+    public Command cmd;
+    public String label;
+    public String[] args;
+
+    public CommandData(CommandSender sender,Command cmd, String label, String[] args){
+        this.sender = sender;
+        this.cmd = cmd;
+        this.label = label;
+        this.args = args;
+    }
+}
