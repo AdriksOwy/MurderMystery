4e0920c88a04adb3c2831eaba19ef2ecefa2045b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-30 21:57:04 +0100

    refactor: SpawnNPC upgrade and now is a CommandRunner implementation

--- src/main/java/com/murder/commands/SpawnNPC.java
+++ src/main/java/com/murder/commands/SpawnNPC.java
@@ -4,18 +4,30 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 
-public class SpawnNPC {
-    World world;
-    Location location;
-    public SpawnNPC(World world, Location location) {
-        this.world = world;
-        this.location = location;
-    }
-    public void spawnMurderVillager(){
+public class SpawnNPC implements CommandRunner {
+    CommandData data;
+
+    private void spawnMurderVillager(World world, Location location){
         Villager villager = (Villager) world.spawnEntity(location, EntityType.VILLAGER);
         villager.setCustomName(ChatColor.YELLOW + "" + ChatColor.BOLD + "> KLIKNIJ PRZYCISK <");
         villager.setAI(false);
+    }
+    public boolean run(){
+        if (data.sender instanceof Player) {
+            Player player = (Player) data.sender;
+            if (player.hasPermission("npc")) {
+                spawnMurderVillager(player.getWorld(), player.getLocation());
+                player.sendMessage(ChatColor.RED + "Villager was created!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public SpawnNPC(CommandData data){
+        this.data = data;
     }
 }
