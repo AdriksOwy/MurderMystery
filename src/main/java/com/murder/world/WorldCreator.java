d61edb540a26ef6ba8475a49fa63abcc8f91e30e
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-01 21:30:43 +0100

    refactor(WorldCreator.java): taking responsibility from WorldManager to WorldCreator.

---
+++ src/main/java/com/murder/world/WorldCreator.java
@@ -0,0 +1,49 @@
+package com.murder.world;
+
+import com.murder.Murder;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class WorldCreator {
+    private JavaPlugin plugin;
+    private String worldName;
+
+    public World createWorld(String world) throws IOException {
+
+        File dataFolder = new File(plugin.getDataFolder().getPath());
+        String strData = dataFolder.toString();
+
+        String[] split = strData.split(File.pathSeparator);
+        String rootFolder = split[split.length - 3];
+
+        File root = new File(rootFolder);
+        File srcDir = new File(root+File.pathSeparator+"backup");
+
+        if (!srcDir.exists()) {
+            Bukkit.getLogger().warning(ChatColor.RED +"Backup does not exist!");
+            throw new IOException("Directory does not exist.");
+        }
+
+        File destDir = new File(root+File.pathSeparator+world);
+
+        try {
+            FileUtils.copyDirectory(srcDir, destDir);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        org.bukkit.WorldCreator creator = new org.bukkit.WorldCreator(world);
+        return Bukkit.getServer().createWorld(creator);
+    }
+
+    public WorldCreator(String worldName) {
+
+        this.worldName = worldName;
+        this.plugin = Murder.getMainPlugin();
+    }
+}
